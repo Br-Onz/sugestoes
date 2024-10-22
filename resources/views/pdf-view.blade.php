3e6ddae7b28adc59aa9d74a1770cd33d66576dd3
fco-assis-jr <franciscoassisjr00@gmail.com>
2024-10-22 13:11:33 -0400

    Formatação PDF

--- resources/views/pdf-view.blade.php
+++ resources/views/pdf-view.blade.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Relatório de Vendas</title>
 
-    <!-- Adiciona a biblioteca Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-7QHTaHP6uMHm4j8DNKwK3J/AdKuCzZYm9L8uT8qSD5O47hpFGEzYFY7Jp1mR9bbZJ/j+cmLNkAiw0OqrNgfDgg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <style>
         body {
@@ -23,7 +21,7 @@
             font-size: 14px;
         }
         .conteudo {
-            margin-top: 40px; /* Ajuste de margem para não sobrepor o texto do topo */
+            margin-top: 40px;
             text-align: center;
         }
         .container {
@@ -53,10 +51,10 @@
             margin-top: 10px;
         }
         .table th, .table td {
-            padding: 2px 4px; /* Reduzido para deixar as linhas mais compactas */
+            padding: 2px 4px;
             text-align: center;
             border: 1px solid #000;
-            line-height: 1; /* Reduz a altura da linha */
+            line-height: 1;
         }
         .table th {
             background-color: #eaeaea;
@@ -67,7 +65,7 @@
         }
         .assinatura {
             position: absolute;
-            bottom: 50px; /* Ajuste da posição do campo de assinatura */
+            bottom: 50px;
             left: 50%;
             transform: translateX(-50%);
             text-align: center;
@@ -77,15 +75,33 @@
             width: 300px;
             border-top: 1px solid #000;
         }
+        .valor {
+            text-align: right;
+            font-family: Arial, sans-serif;
+        }
+        .td-valor {
+            position: relative;
+            font-family: Arial, sans-serif;
+            padding-right: 10px;
+        }
+        .simbolo {
+            position: absolute;
+            left: 2.5;
+        }
+        .valor {
+            text-align: right;
+            display: inline-block;
+            width: 100%;
+        }
     </style>
 </head>
 <body>
-<!-- Texto no topo direito -->
+
 <div class="topo-direito">
     <strong>{{$itensc['pcempr']->usuariobd}} | {{$itensc['pcempr']->matricula}}</strong>
 </div>
 
-<!-- Conteúdo do PDF -->
+
 <div class="conteudo">
     <h1>Sugetão Preço</h1>
     <p></p>
@@ -105,7 +121,7 @@
         </div>
         <div>
             <i class="fa-solid fa-calendar-days icon"></i>
-            <span>Data da Solicitação: {{\Carbon\Carbon::parse($itensc[0]->data)->format('d/m/Y')}}</span>
+            <span>Data da Solicitação: {{\Carbon\Carbon::parse($itensc[0]->data)->format('d/m/Y H:i:s')}}</span>
         </div>
     </div>
 
@@ -128,16 +144,25 @@
         <tbody>
 
         @foreach($itensc['itensi'] as $itensi)
-            <tr>
+            <tr >
 
                 <td>{{$itensi->codprod}}</td>
                 <td>{{$itensi->codauxiliar}}</td>
                 <td>{{$itensi->descricao}}</td>
                 <td>{{$itensi->unidade}}</td>
-                <td>{{$itensi->pvenda}}</td>
-                <td>{{$itensi->poferta}}</td>
-                <td>{{$itensi->valor_sugerido}}</td>
-                <td>{{\Carbon\Carbon::parse($itensi->data_vencimento)->format('d/m/Y') }}</td>
+                <td class="td-valor">
+                    <span class="simbolo">R$</span>
+                    <span class="valor">{{number_format($itensi->pvenda, 2, ',', '.')}}</span>
+                </td>
+                <td class="td-valor">
+                    <span class="simbolo">R$</span>
+                    <span class="valor">{{number_format($itensi->poferta, 2, ',', '.')}}</span>
+                </td>
+                <td class="td-valor">
+                    <span class="simbolo">R$</span>
+                    <span class="valor">{{number_format($itensi->valor_sugerido, 2, ',', '.')}}</span>
+                </td>
+                 <td>{{\Carbon\Carbon::parse($itensi->data_vencimento)->format('d/m/Y') }}</td>
                 <td>{{$itensi->quantidade}}</td>
 
             </tr>
@@ -146,7 +171,7 @@
     </table>
     <div class="assinatura">
         <div class="linha-assinatura"></div>
-        <p>Assinatura</p>
+        <p>{{$itensc['pcempr']->nome}}</p>
     </div>
 </div>
 
